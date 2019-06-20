c2d9d93bc168c32f1a8b8e685cfa62af38d58887
GQSM <ms314006@hotmail.com>
2019-06-20 09:26:23 +0800

    [ADD] Victory day01

---
+++ Day01/src/index.js
@@ -0,0 +1,9 @@
+import position from './position/index.js'
+
+const profile = {
+  name: '神Ｑ超人',
+  position: 'Front End Development',
+};
+
+console.log(`Hi! My name is ${profile.name}`);
+console.log(`position is ${position}`);
