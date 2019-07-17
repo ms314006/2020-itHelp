5b4323457ba53916aa4b1fb9c9cac7a483ff3793
GQSM <ms314006@hotmail.com>
2019-07-18 07:22:07 +0800

    [ADD] 完成 Day04 SCSS 的使用

---
+++ Day04/README.md
@@ -0,0 +1,13 @@
+<h1 align="center">JSX 瀏覽器看不懂？要翻譯就靠 Babel</h1>
+
+## Quick Start
+
+1. Copy the content of root folder to your project folder.
+2. Run the cmd: `npm install` or `npm i`.
+3. Run the cmd: `npm run build`.
+
+## 快速開始
+
+1. 複製根目錄內容到你的專案資料夾。
+2. 在 cmd 中輸入指令 `npm install` 或 `npm i`。
+3. 在 cmd 中輸入指令 `npm run build`。
---
+++ Day04/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /.jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: { localIdentName: '[name]__[local]___[hash:base64:5]' },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './index.css',
+    }),
+  ],
+};
