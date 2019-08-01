a931fccdd61b862602abe78e16c067217459d957
GQSM <ms314006@hotmail.com>
2019-08-01 08:11:53 +0800

    [ADD] 完成 useEffect 的範例

--- Day08/src/index.jsx
+++ Day08/src/index.jsx
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
 import styles from './index.scss';
 
-const Main = () => {
+const Counter = () => {
+  useEffect(() => {
+    console.log('Component Render 後執行');
+    return () => {
+      console.log('Component 移除執行');
+    }
+  }, []);
+  
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(`State 改變前 ${count}`)
+    return () => {
+      console.log(`State 改變後 ${count}`);
+    };
+  }, [count]);
+
   return (
     <>
       <h1 className={styles.main}>{count}</h1>
       <button type="button" onClick={() => { setCount(count + 1); }}>
         點我加 1
       </button>
+    </>
+  );
+};
+
+const Main = () => {
+  const [hiddenCounter, setHiddenCounter] = useState(false);
+  return (
+    <>
+      <button type="button" onClick={() => { setHiddenCounter(!hiddenCounter); }}>
+        開啟、關閉計數器
+      </button>
+      {
+        hiddenCounter ? null : <Counter />
+      }
     </>
   );
 };
